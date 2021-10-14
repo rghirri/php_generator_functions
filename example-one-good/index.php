97d83d464d4fd4fa1c138f5dc1cb20238289f667
Rayaa <rghirri@gmail.com>
2021-10-14 20:48:40 +0100

    completed assocuative and working on matching date

--- example-one-good/index.php
+++ example-one-good/index.php
@@ -3,23 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//$file = 'file.txt';
-// function getLines($file)
-// {
-// $filedata = fopen($file, "r") or die("Unable to open file!");
-// $lineArray = array();
-// while (!feof($filedata)) {
-//     $line = fgets($filedata);
-//     $values = explode(',', $line);
-//     $lineArray[$values[0]] = array('name' => $values[0], 'birthDay' => $values[1]);
-//     yield $lineArray;
-// }
-// fclose($filedata);
-// }
-
-//var_dump($lineArray);
-
-function getLines($file)
+$file = 'file.txt';
+function getEmployeeData($file)
 {
   $myfile = fopen($file, "r") or die("Unable to open file!");
   // Output one line until end-of-file
@@ -39,7 +24,31 @@ function getLines($file)
   }
   fclose($myfile);
 }
+
+//var_dump($lineArray);
+
+// function getEmployeeData($file)
+// {
+//   $myfile = fopen($file, "r") or die("Unable to open file!");
+//   // Output one line until end-of-file
+//   $lineArray = array();
+//   while(!feof($myfile)) {
+//     $str = fgets($myfile);
+//     $employeeDateArray = explode(",",$str);
+//     $lineArray = array('name' => $employeeDateArray[0], 'birthDay' => $employeeDateArray[1]);
+
+//     yield $lineArray;
+    
+    // $birthDate = $employeeDateArray['1'];
+    // $dateOfBirth = new \DateTime($birthDate);
+    // $dateOfBirth = $dateOfBirth->format('m-d');
+    // var_dump($dateOfBirth);
   
+//   }
+//   fclose($myfile);
+// }
+  
+
 // $file = fopen($file, 'r');
 // $headers = fgetcsv($file);
 // $result = array();
@@ -68,19 +77,38 @@ function getLines($file)
 //   fclose($fp);
 // }
 
-// function birthdayMatch($line)
-// {
-//   $pattern = "/06-25/i";
-//   if (preg_match_all($pattern, $line)){
-//     echo "It's working" . $line;
-//   }
+function birthdayMatch($line)
+{
+ $pat = "06-25";
+ $pattern = "/".$pat."/i";
+ $birthDate = $line['birthDay'];
+ $dateOfBirth = new \DateTime($birthDate);
+ $dateOfBirth = $dateOfBirth->format('m-d');
+  if (preg_match_all($pattern, $dateOfBirth)){
+      echo $line['name']. " date of birth " .$line['birthDay'];
+    }
+}
+
+// function datePattern(){
+//   $myfile = fopen("file.txt", "r") or die("Unable to open file!");
+// // Output one line until end-of-file
+// while(!feof($myfile)) {
+//   $str = fgets($myfile);
+//   $employeeDateArray = explode(",",$str);
+//   $birthDate = $employeeDateArray['1'];
+//   $dateOfBirth = new \DateTime($birthDate);
+//   $dateOfBirth = $dateOfBirth->format('m-d');
+//   yield $dateOfBirth;
+
+// }
+// fclose($myfile);
 // }
 
-$file = 'file.txt';
-$lineGenerator = getLines($file);
+//$file = 'file.txt';
+$lineGenerator = getEmployeeData($file);
 foreach ($lineGenerator as $line) {
-  //birthdayMatch($line);
-  var_dump($line);
+  birthdayMatch($line);
+   //var_dump($line);
 }
 
 /************** This works Iterator ****************** */
