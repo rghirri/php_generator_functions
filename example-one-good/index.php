3b1747f55f2ff97f6761fdec97a36987e55eec83
Rayaa <rghirri@gmail.com>
2021-10-14 18:50:29 +0100

    Turn txt file to associative array

--- example-one-good/index.php
+++ example-one-good/index.php
@@ -3,32 +3,84 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$file = 'file.txt';
+//$file = 'file.txt';
+// function getLines($file)
+// {
+// $filedata = fopen($file, "r") or die("Unable to open file!");
+// $lineArray = array();
+// while (!feof($filedata)) {
+//     $line = fgets($filedata);
+//     $values = explode(',', $line);
+//     $lineArray[$values[0]] = array('name' => $values[0], 'birthDay' => $values[1]);
+//     yield $lineArray;
+// }
+// fclose($filedata);
+// }
 
-/************** This works Generator ****************** */
+//var_dump($lineArray);
+
 function getLines($file)
 {
-  $fp = fopen($file, 'r');
+  $myfile = fopen($file, "r") or die("Unable to open file!");
+  // Output one line until end-of-file
+  $lineArray = array();
+  while(!feof($myfile)) {
+    $str = fgets($myfile);
+    $employeeDateArray = explode(",",$str);
+    $lineArray = array('name' => $employeeDateArray[0], 'birthDay' => $employeeDateArray[1]);
 
-  while (!feof($fp)) {
-    $line = fgets($fp);
-    yield $line;
+    yield $lineArray;
+    
+    // $birthDate = $employeeDateArray['1'];
+    // $dateOfBirth = new \DateTime($birthDate);
+    // $dateOfBirth = $dateOfBirth->format('m-d');
+    // var_dump($dateOfBirth);
+  
   }
-  fclose($fp);
+  fclose($myfile);
 }
+  
+// $file = fopen($file, 'r');
+// $headers = fgetcsv($file);
+// $result = array();
+// while ($row = fgetcsv($file)) {
+//         if (!$row[0]) continue;
+//         $nextItem = array();
+//         for ($i = 0; $i < 1; ++$i) {
+//                 $nextItem[$headers[$i]] = $row[$i];
+//         }
+//         $result[] = $nextItem;
+//         yield $result;
+// }
+// fclose($file);
+// }
+//var_dump($result);
 
-function birthdayMatch($line)
-{
-  $pattern = "/06-25/i";
-  if (preg_match_all($pattern, $line)){
-    echo "It's working" . $line;
-  }
-}
+/************** This works Generator ****************** */
+// function getLines($file)
+// {
+//   $fp = fopen($file, 'r');
 
+//   while (!feof($fp)) {
+//     $line = fgets($fp);
+//     yield $line;
+//   }
+//   fclose($fp);
+// }
 
+// function birthdayMatch($line)
+// {
+//   $pattern = "/06-25/i";
+//   if (preg_match_all($pattern, $line)){
+//     echo "It's working" . $line;
+//   }
+// }
+
+$file = 'file.txt';
 $lineGenerator = getLines($file);
 foreach ($lineGenerator as $line) {
-  birthdayMatch($line);
+  //birthdayMatch($line);
+  var_dump($line);
 }
 
 /************** This works Iterator ****************** */
